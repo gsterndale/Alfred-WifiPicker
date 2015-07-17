119cc1035724d05f78a9b3106ec3cdec4223c0cc
Anthony Townsend <anthony@bitsandatoms.net>
2015-07-16 22:07:32 -0400

    FINISHED
    Everything works. Yay. Need to filter out multiple entires for SSIDs
    broadcast by multiple access points.

--- GetNetworks.py
+++ GetNetworks.py
@@ -2,36 +2,27 @@
 # grabs list of current WiFi networks and dumps it back to Alfred feedback
 
 # TO DO ADD BACK when figure out to set an encryption flag properly
-
 #   def get_network_icon(encryption):
 #       if encryption == 'NONE':
 #           return 'OPEN.png'
 #       else:
 #           return 'CLOSED.png'
 
-
 import os, sys, WifiScan
 from Feedback import Feedback
 def run():
     
-    # networks is a dictionary that contains list of MAC:SSID pairs
-    # but its coming back empty -- hence the {}
-
+    # scan for available networks
     networks = WifiScan.find_access_points()
-
-    print >> sys.stderr, "This is GetNetworks.py"
-    print >> sys.stderr, networks
-    
 
     # Create the object to display networks
     feedback = Feedback()
 
     # Add the network items to feedback
-    
     for mac in networks:
         
         # replace icon with network_icon later (see subroutine at bototm)
-        feedback.add_item(networks[mac], mac, '', icon='OPEN.png')
+        feedback.add_item(networks[mac], mac, networks[mac], icon='OPEN.png')
         
     return feedback
         
